f0a4da8e34b830caf0bc467ae8e0c8463cee4b33
Rahul <rahulatrkm@gmail.com>
2020-07-04 14:15:03 +0530

    Completed all sections

--- app/src/main/java/com/example/singlescreenapp/MainActivity.java
+++ app/src/main/java/com/example/singlescreenapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.singlescreenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLowes(View view) {
+        String url = "https://www.lowes.com";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void lowesfb(View view) {
+        String url = "https://www.facebook.com/lowes";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void lowesYt(View view) {
+        String url = "https://www.facebook.com/lowes";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void lowesInsta(View view) {
+        String url = "https://instagram.com/loweshomeimprovement";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void lowesPinterest(View view) {
+        String url = "https://www.pinterest.com/lowes";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void lowesTwitter(View view) {
+        String url = "https://twitter.com/lowes";
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
